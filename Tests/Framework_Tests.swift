91e3885063bbe196a8b0687eaddb37193cd64234
Cihat Gündüz <CihatGuenduez@posteo.de>
2015-12-30 15:45:04 +0100

    Add Carthage and Framework support

---
+++ Tests/Framework_Tests.swift
@@ -0,0 +1,21 @@
+//
+//  Framework_Tests.swift
+//  Framework Tests
+//
+//  Created by Cihat Gündüz on 30.12.15.
+//  Copyright © 2015 Jeff Menter. All rights reserved.
+//
+
+import XCTest
+
+@testable import JAMSVGImage
+
+class JAMSVGImage_Tests: XCTestCase {
+    
+    func testExample() {
+        
+        // prove that tests are working
+        XCTAssertTrue(true)
+        
+    }
+}
